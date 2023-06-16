beba0a4b4115749383223116f54ac6e705e61826
Mario <mario.vieilledent@gmail.com>
2023-06-17 00:16:00 +0200

    ui

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,59 @@
-console.log('uwu');
+let eventDiv;
+let events = [];
+
+function load() {
+    eventDiv = document.getElementById('event-list');
+    getEvents();
+}
+
+function getEvents() {
+    fetch(window.location.href + 'getEvents')
+        .then(res => res.json())
+        .then(data => {
+            events = data;
+            displayEvents();
+        });
+}
+
+function addEvent(event) {
+    fetch(window.location.href + 'addEvent', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(event)
+    }).then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function displayEvents() {
+    events.forEach(event => {
+        const starts = new Date(event.starts);
+        const ends = new Date(event.starts);
+
+        let str = '';
+        str += `<div class="event">`;
+        str += `<div class="event-head">`;
+        str += `<div class="event-head-left">`;
+
+        str += `<span class="event-date">`;
+        str += `${('0' + starts.getDate()).slice(-2)}/${('0' + (starts.getMonth() + 1)).slice(-2)}`;
+        str += `</span>`;
+        str += `<span class="event-time">`;
+        str += `à ${('0' + starts.getHours()).slice(-2)}:${('0' + starts.getMinutes()).slice(-2)}`;
+        str += `</span>`;
+
+        str += `</div>`;
+        str += `<div class="event-head-right">`;
+
+        str += `<span class="event-title">`;
+        event.name ? str += event.name : str += 'Sans titre :c';
+        str += `</span>`;
+        str += `<span class="event-description">`;
+        event.description ? str += event.description : str += 'Sans titre :c';
+        str += `</span>`;
+
+        str += `</div>`;
+        str += `</div>`;
+        str += `</div>`;
+        eventDiv.innerHTML += str;
+    });
+}
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const port = 3000;
+
+let events = [];
 
 app.use(express.static('dist'));
 
-app.listen(3000);
+app.get('/getEvents', (req, res) => {
+    console.log('/getEvents');
+    fs.readFile('./events.json', (err, data) => {
+        if (err) {
+            res.send([]);
+        } else {
+            events = JSON.parse(data)
+            res.send(events);
+        }
+    });
+});
+
+app.listen(port);
 console.log('ok');
